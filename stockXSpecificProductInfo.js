7aad07e5df9bbad2f5d5f945362c65242ba3ce77
Arpon Purkayastha <arponp30@gmail.com>
2020-08-29 16:22:02 -0700

    Send mockdata response

--- stockXSpecificProductInfo.js
+++ stockXSpecificProductInfo.js
@@ -9,11 +9,33 @@ const stockX = new StockXAPI({
 app.get('/stockx/product',async (req,res) => {
   // let allProducts = []
   // console.log(req.query.keyword)
-  let urlKey = req.query.urlKey
-  let productList = await stockX.fetchProductDetails(urlKey);
-  res.send(productList)
+  // let urlKey = req.query.urlKey
+  // let productList = await stockX.fetchProductDetails(urlKey);
+  // res.send(productList)
+
+  //mock data
+  var mockList = {
+    name: "Jordan 4 Retro Union Guava Ice",
+    urlKey: "air-jordan-4-retro-union-guava-ice",
+    thumbnail_url: "https://stockx.imgix.net/Air-Jordan-4-Retro-Union-Guava-Ice.png?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1596306060",
+    pid: "DC9533-800",
+    variants: []
+  }
+  for (var i=12; i<=15; i++) {
+    var mockVariant = new Object()
+    mockVariant.size = `${i}`
+    mockVariant.uuid = "306fa326-767e-4bd6-a010-e78228cfe30d"
+    mockVariant.market = {
+      lowestAsk: 800,
+      highestBid: 700
+    }
+    mockList.variants.push(mockVariant)
+  }
+
+  mockList = JSON.stringify(mockList)
+  res.send(mockList)
 })
 
 
 
-const server = app.listen(3000, function() {console.log('api connected')});
+const server = app.listen(3001, function() {console.log('api connected')});
